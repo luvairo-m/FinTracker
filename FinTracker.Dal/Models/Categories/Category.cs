47c27f7f27da0ff2f39238b83ed88bbefd1d6e41
luvairo-m <mihail-shutov@inbox.ru>
2024-12-24 23:58:48 +0500

    Репозиторий категорий + тесты.

---
+++ FinTracker.Dal/Models/Categories/Category.cs
@@ -0,0 +1,22 @@
+﻿using FinTracker.Dal.Logic.Attributes;
+
+namespace FinTracker.Dal.Models.Categories;
+
+/// <summary>
+/// Категория платежа.
+/// </summary>
+[Table(Name = "Category", Schema = "dbo")]
+public class Category
+{
+    /// <summary>
+    /// Идентификатор категории.
+    /// </summary>
+    [Column(Name = "Id")]
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Название категории.
+    /// </summary>
+    [Column(Name = "Title")]
+    public string Title { get; set; }
+}
---
+++ FinTracker.Dal/Models/Categories/CategoryFilter.cs
@@ -0,0 +1,36 @@
+﻿using FinTracker.Dal.Logic.Utils;
+
+namespace FinTracker.Dal.Models.Categories;
+
+public class CategoryFilter
+{
+    public Guid? Id { get; set; }
+    
+    public string SearchText { get; set; }
+
+    public string ToWhereExpression()
+    {
+        var whereCollector = new List<string>(2);
+
+        if (this.Id != null)
+        {
+            whereCollector.Add($"Id = @{nameof(this.Id)}");
+        }
+
+        if (this.SearchText != null)
+        {
+            whereCollector.Add($"Title LIKE @{nameof(this.SearchText)}");
+        }
+
+        return SqlRequestUtils.BuildWhereExpression(whereCollector);
+    }
+
+    public Dictionary<string, object> ToParametersWithValues()
+    {
+        return new Dictionary<string, object>
+        {
+            [nameof(this.Id)] = this.Id,
+            [nameof(this.SearchText)] = $"%{this.SearchText}%"
+        };
+    }
+}
